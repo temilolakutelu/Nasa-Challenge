b2cd0aa59a9f669323e9f22b8723a33155c59875
temilolakutelu <temilolakutelu@gmail.com>
2022-05-05 19:07:55 +0100

    integration of nasa planetary using axios

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,20 +2,50 @@
   <div class="header container-fluid">
     <h1 class="headerText">
       <span>NASA:</span> Picture of the Day
-      <div>Thursday, May 5, 2022</div>
+      <div>{{formatDate(nasa_data.date)}}</div>
     </h1>
     <img
+      :title="nasa_data.title"
       class="headerImage img-fluid img-responsive W-100"
-      src="https://apod.nasa.gov/apod/image/2205/NGC3521LRGBHaAPOD-20.jpg"
+      :src="nasa_data.hdurl"
     />
   </div>
 </template>
 
+
 <script>
+import axios from 'axios';
 export default {
   name: 'TopHeader',
-  props: {
-    msg: String
+  data () {
+    return {
+      nasa_data: {}
+    }
+  },
+  mounted () {
+    this.getNasaData();
+  },
+  methods: {
+    //api to fetch header data
+    getNasaData () {
+      let vm = this;
+      axios.get('https://api.nasa.gov/planetary/apod?api_key=SPkLKA7bCBamNIY9kJ4ceIeWB67uFjxP5lXkQeNR', {
+      })
+        .then(function (response) {
+          console.log(response.data);
+          vm.nasa_data = response.data
+        })
+    },
+    formatDate (date) {
+      if (date !== null) {
+        let d = new Date(date)
+        return d.toDateString().replace(' ', ', ')
+      }
+      else {
+        return null
+      }
+    },
+
   }
 }
 </script>
